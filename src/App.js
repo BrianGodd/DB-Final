2c59b52a4d81191a9bda570d91710cc47736c65f
BrianGodd <brianhuang0083.cs11@nycu.edu.tw>
2024-01-02 04:31:04 +0800

    20240102

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p style={{position: 'absolute', top: '10px', right: '10px'}}>Hello World!</p>
+      <p style={{position: 'absolute', top: '10px', left: '700px'}}>Hello World!</p>
     </div>
   );
 }
